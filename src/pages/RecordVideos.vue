e3e677263849febed3b5a5c28c44926d2711b216
kaankoro <kaankoroglu@recram.com>
2024-07-31 13:59:10 +0300

    debugging

--- src/pages/RecordVideos.vue
+++ src/pages/RecordVideos.vue
@@ -111,6 +111,7 @@ const saveVideo = async () => {
 };
 
 const isMobile = () => {
+  console.log(navigator.userAgent)
   return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
 }
 
